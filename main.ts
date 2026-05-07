8ca0d36389bd9903af8863b7c104469da5c26230
Hayley Witherell <hayleywitherell@gmail.com>
2026-05-06 20:44:35 -0600

    Add repeating combat turn loop

--- main.ts
+++ main.ts
@@ -26,17 +26,25 @@ function randomDamage(max: number): number {
   return Math.floor(Math.random() * max) + 1;
 }
 
-const sampleHit = randomDamage(player.attack);
-goblin.hp = goblin.hp - sampleHit;
-const goblinHit = randomDamage(goblin.attack);
-player.hp = player.hp - goblinHit;
-
 console.log("Player:", player);
 console.log("Enemy:", goblin);
-console.log(`You hit the goblin for ${sampleHit} damage.`);
-console.log(`Goblin HP: ${goblin.hp}`);
-console.log(`Goblin hits you for ${goblinHit} damage.`);
-console.log(`Player HP: ${player.hp}`);
+
+while (player.hp > 0 && goblin.hp > 0) {
+  const playerHit = randomDamage(player.attack);
+  goblin.hp = goblin.hp - playerHit;
+  console.log(`You hit the goblin for ${playerHit} damage.`);
+  console.log(`Goblin HP: ${goblin.hp}`);
+
+  if (goblin.hp <= 0) {
+    break;
+  }
+
+  const goblinHit = randomDamage(goblin.attack);
+  player.hp = player.hp - goblinHit;
+  console.log(`Goblin hits you for ${goblinHit} damage.`);
+  console.log(`Player HP: ${player.hp}`);
+  console.log("---");
+}
 
 if (goblin.hp <= 0) {
   console.log("You win!");
